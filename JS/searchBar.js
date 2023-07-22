e515ec879db7f98af83c44775866236d6f1ae72b
spoorthi <spoorthiachar9@gmail.com>
2023-07-22 21:47:29 +0530

    final commit

--- JS/searchBar.js
+++ JS/searchBar.js
@@ -1,41 +1,41 @@
 // searchBar.js
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('searchInput');
-    const searchResults = document.getElementById('searchResults');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const searchInput = document.getElementById('searchInput');
+//     const searchResults = document.getElementById('searchResults');
   
-    searchInput.addEventListener('input', handleSearch);
+//     searchInput.addEventListener('input', handleSearch);
   
-    function handleSearch() {
-      const query = searchInput.value.toLowerCase();
-      const results = performSearch(query);
-      displayResults(results);
-    }
+//     function handleSearch() {
+//       const query = searchInput.value.toLowerCase();
+//       const results = performSearch(query);
+//       displayResults(results);
+//     }
   
-    function performSearch(query) {
-      // Example search logic
-      const products = [
-        'Product 1',
-        'Product 2',
-        'Product 3',
-        'Another Product',
-        'Some Product'
-      ];
+//     function performSearch(query) {
+//       // Example search logic
+//       const products = [
+//         'Product 1',
+//         'Product 2',
+//         'Product 3',
+//         'Another Product',
+//         'Some Product'
+//       ];
   
-      const results = products.filter(product =>
-        product.toLowerCase().includes(query)
-      );
+//       const results = products.filter(product =>
+//         product.toLowerCase().includes(query)
+//       );
   
-      return results;
-    }
+//       return results;
+//     }
   
-    function displayResults(results) {
-      searchResults.innerHTML = '';
+//     function displayResults(results) {
+//       searchResults.innerHTML = '';
   
-      results.forEach(result => {
-        const resultElement = document.createElement('p');
-        resultElement.textContent = result;
-        searchResults.appendChild(resultElement);
-      });
-    }
-  });
+//       results.forEach(result => {
+//         const resultElement = document.createElement('p');
+//         resultElement.textContent = result;
+//         searchResults.appendChild(resultElement);
+//       });
+//     }
+//   });
   
